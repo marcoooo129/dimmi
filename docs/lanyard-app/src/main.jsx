d521b4ac813089bddb5ba2eeef0c0dd3487147d7
Marco Su <marcoooo129@example.com>
2026-07-17 19:28:59 +0200

    feat: fix OptionWheel text syntax and integrate

--- docs/lanyard-app/src/main.jsx
+++ docs/lanyard-app/src/main.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Lanyard from './Lanyard'
+import OptionWheelDemo from './OptionWheelDemo'
 
-ReactDOM.createRoot(document.getElementById('lanyard-root')).render(
-  <React.StrictMode>
-    <Lanyard 
-      position={[0, 0, 24]} 
-      gravity={[0, -40, 0]} 
-      frontImage="assets/marketing/app-icon.png"
-      imageFit="contain"
-    />
-  </React.StrictMode>,
-)
+const lanyardRoot = document.getElementById('lanyard-root');
+if (lanyardRoot) {
+  ReactDOM.createRoot(lanyardRoot).render(
+    <React.StrictMode>
+      <Lanyard 
+        position={[0, 0, 24]} 
+        gravity={[0, -40, 0]} 
+        frontImage="assets/marketing/app-icon.png"
+        imageFit="contain"
+      />
+    </React.StrictMode>,
+  )
+}
+
+const optionWheelRoot = document.getElementById('option-wheel-root');
+if (optionWheelRoot) {
+  ReactDOM.createRoot(optionWheelRoot).render(
+    <React.StrictMode>
+      <OptionWheelDemo />
+    </React.StrictMode>,
+  )
+}
